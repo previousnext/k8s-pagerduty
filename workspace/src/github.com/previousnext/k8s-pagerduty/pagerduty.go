3e314c62559b32e720540effba588c0f6c41298d
Nick Santamaria <nick.santamaria@previousnext.com.au>
2017-08-31 14:38:40 +1000

    Added logging to PagerDuty event API call.

--- workspace/src/github.com/previousnext/k8s-pagerduty/pagerduty.go
+++ workspace/src/github.com/previousnext/k8s-pagerduty/pagerduty.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"github.com/PagerDuty/go-pagerduty"
+	log "github.com/Sirupsen/logrus"
 )
 
 func pagerdutyEvent(action, serviceKey, indicentKey, description string) error {
+	log.Infof("PagerDuty %s incident - %s", action, description)
 	_, err := pagerduty.CreateEvent(pagerduty.Event{
 		Type:        action,
 		ServiceKey:  serviceKey,
