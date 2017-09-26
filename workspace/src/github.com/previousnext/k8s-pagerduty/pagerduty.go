f703ffcbc6a03e2d67929b84d9041e3f0e78fd0b
Nick Schuch <nick@myschuch.com>
2017-09-26 16:27:39 +1000

    Adds retries and Prometheus

--- workspace/src/github.com/previousnext/k8s-pagerduty/pagerduty.go
+++ workspace/src/github.com/previousnext/k8s-pagerduty/pagerduty.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"log"
+
 	"github.com/PagerDuty/go-pagerduty"
+	"github.com/cenkalti/backoff"
 )
 
-func pagerdutyEvent(action, serviceKey, indicentKey, description string) error {
-	_, err := pagerduty.CreateEvent(pagerduty.Event{
-		Type:        action,
-		ServiceKey:  serviceKey,
-		IncidentKey: indicentKey,
-		Description: description,
-	})
-	return err
+func pagerdutyEvent(action, serviceKey, indicentKey, description string) (*pagerduty.EventResponse, error) {
+	ticker := backoff.NewTicker(backoff.NewExponentialBackOff())
+
+	var (
+		resp *pagerduty.EventResponse
+		err  error
+	)
+
+	for range ticker.C {
+		resp, err = pagerduty.CreateEvent(pagerduty.Event{
+			Type:        action,
+			ServiceKey:  serviceKey,
+			IncidentKey: indicentKey,
+			Description: description,
+		})
+		if err != nil {
+			log.Printf("Failed to retrieve org members, retrying: %s", err)
+			continue
+		}
+
+		ticker.Stop()
+		break
+	}
+
+	return resp, err
 }
